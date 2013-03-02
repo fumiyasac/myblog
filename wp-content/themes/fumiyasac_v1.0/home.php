3801ed7a9be07eeca78cffcdbd7fb05b767c2193
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2013-03-03 00:48:33 +0900

    記述をもとに戻した（git確認用）

--- wp-content/themes/fumiyasac_v1.0/home.php
+++ wp-content/themes/fumiyasac_v1.0/home.php
@@ -9,7 +9,8 @@ Author URI: http://blog.just1factory.net
 Template: home.php
 Version: 1.0
 */ 
-
+?>
+<?php
 /* いらないCSSを削除 */
 if(has_action('wp_head','_admin_bar_bump_cb')){
 remove_action('wp_head','_admin_bar_bump_cb');
